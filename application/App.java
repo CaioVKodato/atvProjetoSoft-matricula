0a0e12e409927f85e90bcc543689cf332a7ea3a8
CaioVKodato <caiovictorkodatot@gmail.com>
2025-03-10 22:45:38 -0300

    Teste do curriculo sendo gerado no app

--- application/App.java
+++ application/App.java
@@ -5,19 +5,23 @@ import entities.Curso;
 import entities.Disciplina;
 import entities.Matricula;
 import entities.Professor;
+import entities.Secretaria;
 import entities.Semestre;
 
 public class App {
     public static void main(String[] args) {
         Curso c1 = new Curso("Engenharia de Software", 80);
         Aluno a1 = new Aluno("João", "q8o5w@example.com", "1234");
-        
-        c1.addDisciplina(new Disciplina(c1, "POO", true, true, null));
-        c1.addDisciplina(new Disciplina(c1, "BD", true, true, null));
-        c1.addDisciplina(new Disciplina(c1, "POO2", true, true, null));
-        c1.addDisciplina(new Disciplina(c1, "POO3", true, true, null));
-        c1.addDisciplina(new Disciplina(c1, "BD2", true, true, null));
-        c1.addDisciplina(new Disciplina(c1, "BD3", false, true, null));
+
+        Secretaria sec1 = new Secretaria("Secretaria", "sec@gmail.com", "123");
+        sec1.addCurso(c1);
+        Professor p1 = new Professor("Joaquim", "q8o5w@example.com", "1234");
+        c1.addDisciplina(new Disciplina(c1, "POO", true, true, p1));
+        c1.addDisciplina(new Disciplina(c1, "BD", true, true, p1));
+        c1.addDisciplina(new Disciplina(c1, "POO2", true, true, p1));
+        c1.addDisciplina(new Disciplina(c1, "POO3", true, true, p1));
+        c1.addDisciplina(new Disciplina(c1, "BD2", true, true, p1));
+        c1.addDisciplina(new Disciplina(c1, "BD3", false, true, p1));
 
         Semestre s1 = new Semestre("2025.1");
 
@@ -34,7 +38,7 @@ public class App {
        System.out.println(m1.getNumObrigatorias());
        m1.matricularEmDisciplina(c1.getDisciplinas().get(4));
 
-       Professor p1 = new Professor("Joaquim", "q8o5w@example.com", "1234");
+   
        c1.getDisciplinas().get(0).setProfessor(p1);
        c1.getDisciplinas().get(1).setProfessor(p1);
 
@@ -43,6 +47,10 @@ public class App {
 
        m1.cancelarMatricula(c1.getDisciplinas().get(3));
        System.out.println(m1.getDisciplinas());
+
+       sec1.gerarCurriculo();
+
+      
        
        
 
